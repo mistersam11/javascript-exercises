59a2d6c0bccc91abfdb3600a075b6060a21ca119
Sam <samroth011@gmail.com>
2025-09-09 12:06:28 -0400

    Complete javascript 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split('');
+    const reversed = array.reverse();
+    return reversed.join('');
 };
 
 // Do not edit below this line
