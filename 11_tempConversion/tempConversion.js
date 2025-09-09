eecb380375ab432d6af9e01d31e938129cff1ba8
Sam <samroth011@gmail.com>
2025-09-09 13:19:10 -0400

    Complete javascript 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celsius = ((temp - 32) * (5/9))
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheit = (temp * (9/5) + 32)
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
