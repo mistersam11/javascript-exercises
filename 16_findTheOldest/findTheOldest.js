098a32a84fa8740b02d72087fc40c42d245c4c99
Sam <samroth011@gmail.com>
2025-09-16 15:51:07 -0400

    Complete javascript 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const date = new Date();
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].yearOfDeath === undefined) {
+            arr[i].yearOfDeath = date.getFullYear();
+        }
+        arr[i].age = arr[i].yearOfDeath - arr[i].yearOfBirth;
+    }
+    let sorted = arr.sort((a, b) => b.age - a.age);
+    return sorted[0];
 };
 
 // Do not edit below this line
