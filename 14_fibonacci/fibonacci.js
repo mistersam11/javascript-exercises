cc3ea5fa3fe911949efd94837d7d9a0418920d5d
Sam <samroth011@gmail.com>
2025-09-16 15:30:03 -0400

    Complete javascript 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let array = [1, 1];
+    let cleanNum = parseInt(num);
+    if (cleanNum === 0) {
+        return 0;
+    } else if (cleanNum < 0) {
+        return 'OOPS';
+    } else {
+        for (let i = 2; i < cleanNum; i++) {
+        array.push(array[i - 2] + array[i - 1]);
+        }
+    }
+    return array[array.length-1];
 };
 
 // Do not edit below this line
