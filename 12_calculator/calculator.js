3553df80f3f5dd68dfb3434596769885c6266755
Sam <samroth011@gmail.com>
2025-09-16 14:57:47 -0400

    Complete javascript 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prev, curr) => prev * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let total = 1;
+    for (let i = num; i > 0; i--) {
+      total = total * i;
+    }
+    return total;
 };
 
 // Do not edit below this line
